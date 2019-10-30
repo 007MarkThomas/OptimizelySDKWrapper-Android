48c7d92eb9fbf8cbf34f5b2f034bd431eb2ad950
007MarkThomas <53478097+007MarkThomas@users.noreply.github.com>
2019-10-30 13:09:15 +0000

    Update OptimizelyExperiments.java

--- OptimizelyExperiments.java
+++ OptimizelyExperiments.java
@@ -3,6 +3,7 @@ package com.practice.androidoptimizely;
 
 /**
  * This class stores the names of the experiments
+ * This is also an example of how to structure the experiments
  */
 
 public class OptimizelyExperiments {
