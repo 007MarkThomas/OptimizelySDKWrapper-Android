9a541b5073f213aa7d6880488e92a555c5a4a94e
007MarkThomas <53478097+007MarkThomas@users.noreply.github.com>
2019-10-30 13:09:46 +0000

    Update OptimizelyFeatures.java

--- OptimizelyFeatures.java
+++ OptimizelyFeatures.java
@@ -2,6 +2,7 @@ package com.practice.androidoptimizely;
 
 /**
  * This class stores the names of the features
+ * This is also an example of how to structure your features
  */
 
 public class OptimizelyFeatures {
